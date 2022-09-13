9ac9f4a12c0d7bfadff450253270526d3b107e8a
Sebastian <juseser@gmail.com>
2022-09-13 11:31:14 -0500

    Se agrega el proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,196 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-container>
+      <v-btn width="200" height="50" @click="cargarCajaTexto()">
+        <v-icon>mdi-text-box</v-icon>
+        Caja de Texto
+      </v-btn>
+      <v-btn width="200" height="50" @click="cargarNumero()">
+        <v-icon>mdi-numeric</v-icon>
+        Numero
+      </v-btn>
+      <v-btn width="200" height="50" @click="cargarCheck()">
+        <v-icon>mdi-checkbox-marked</v-icon>
+        CheckBox
+      </v-btn>
+      <v-btn width="200" height="50" @click="cargarRadio()">
+        <v-icon>mdi-radiobox-marked</v-icon>
+        Radio
+      </v-btn>
+      <v-btn width="200" height="50" @click="cargarSelect()">
+        <v-icon>mdi-form-dropdown</v-icon>
+        Select
+      </v-btn>
+      <v-btn width="200" height="50" @click="cargarBoton()"> 
+        <v-icon>mdi-send</v-icon>
+        Botón
+      </v-btn>
+      <v-btn width="200" height="50" @click="cargarTexto()">
+        <v-icon>mdi-alphabetical</v-icon>
+        Texto
+      </v-btn>
+      <v-btn width="200" height="50" @click="exportar()">
+        <v-icon>mdi-download</v-icon>
+      </v-btn>
+      <v-app>
+        <FormularioDinamico :campos="campos"/>
+      </v-app>
+    </v-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import FormularioDinamico from './components/FormularioDinamico.vue';
+    export default{
+        name: 'App',
+        components:{
+          FormularioDinamico
+    },
+    data(){
+      return {
+          campos: [
+                
+          ]
+      };
+    },
+    methods:{
+      cargarCajaTexto(){
+        if(this.campos.length==0){
+          this.campos= [
+            {
+              etiqueta: "v-text-field",
+              label: "Caja de Texto",
+              valor: "",
+              type: "text"
+            }
+          ]
+        }
+        else{
+          this.campos.push(
+            {
+              etiqueta: "v-text-field",
+              label: "Caja de Texto",
+              valor: "",
+              type: "text"
+            }
+          )
+        }
+      },
+      cargarNumero(){
+        if(this.campos.length==0){
+          this.campos=[
+            {
+              etiqueta: "v-text-field",
+              label: "Número",
+              valor: "",
+              type: "number"
+            }
+          ]
+        }
+        else{
+          this.campos.push(
+            {
+              etiqueta: "v-text-field",
+              label: "Número",
+              valor: "",
+              type: "number"
+            }
+          )
+        }
+      },
+      cargarCheck(){
+        if(this.campos.length==0){
+          this.campos=[
+            {
+              etiqueta: "v-checkbox",
+              label: "CheckBox",
+              valor: "",
+            }
+          ]
+        }
+        else{
+          this.campos.push(
+            {
+              etiqueta: "v-checkbox",
+              label: "CheckBox",
+              valor: "",
+            }
+          )
+        }
+      },
+      cargarBoton(){
+        if(this.campos.length==0){
+          this.campos=[
+            {
+              etiqueta: "v-btn",
+              label: "Boton",
+              valor: "Botón",
+            }
+          ]
+        }
+        else{
+          this.campos.push(
+            {
+              etiqueta: "v-btn",
+              label: "Boton",
+              valor: "Botón",
+            }
+          )
+        }
+      }, 
+      cargarRadio(){
+        if(this.campos.length==0){
+          this.campos=[
+            {
+              etiqueta: "v-radio",
+              label: "Radio",
+            }
+          ]
+        }
+        else{
+          this.campos.push(
+            {
+              etiqueta: "v-radio",
+              label: "Radio",
+            }
+          )
+        }
+      },
+      cargarSelect(){
+        if(this.campos.length==0){
+          this.campos=[
+            {
+              etiqueta: "v-select",
+              label: "Select",
+              items:["Test1","Test2","Test3"],
+              type: "text",
+              valor:"Test1"
+            }
+          ]
+        }
+        else{
+          this.campos.push(
+            {
+              etiqueta: "v-select",
+              label: "Select",
+              items:["Test1","Test2","Test3"],
+              type: "text",
+              valor:"Test1"
+            }
+          )
+        }
+      },
+      cargarTexto(){
+        this.campos.push({
+          etiqueta: "v-card-text",
+          label: "Texto"
+        })
+      },
+      exportar(){
+        console.log(JSON.stringify(this.campos))
+      }
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/FormularioDinamico.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <v-form @submit.prevent="" class="form d-flex flex-column">
+            <span class="message" v-if="!campos.length">No hay campos que mostrar</span>
+            <component
+                    v-for="(campo,index) in campos" :key="index"
+                    :is="campo.etiqueta"
+                    :label="campo.label ? campo.label: ''"
+                    :type="campo.type ? campo.type : ''"
+                    :items="campo.items ? campo.items : ''"
+                    v-model="campo.valor"
+            >
+            </component>
+        </v-form>
+    </div>
+</template>
+
+<script>
+    export default{
+        props:['campos']
+    }
+</script>
